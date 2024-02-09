ca64d4f4bae8735b929f68e1105c8f1d452e872f
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2024-02-09 12:12:00 +0200

    .

--- application_windows/my_widgets/switch_btns_frame.py
+++ application_windows/my_widgets/switch_btns_frame.py
@@ -8,21 +8,22 @@ class SwitchBtnsFrame(customtkinter.CTkFrame):
         self.width = 200
         self.height = 50
 
-        prev_month_btn = customtkinter.CTkButton(
+        self.prev_month_btn = customtkinter.CTkButton(
             self, text="prev", width=60, command=calendar_obj.decrease_date
         )
-        prev_month_btn.grid(row=0, column=0, padx=(0, 50), pady=10)
+        self.prev_month_btn.grid(row=0, column=0, padx=(0, 50), pady=10)
 
-        today_btn = customtkinter.CTkButton(
+        self.today_btn = customtkinter.CTkButton(
             self, text="today",
             width=60,
             command=calendar_obj.to_today
-        ).grid(row=0, column=1, pady=10)
+        )
+        self.today_btn.grid(row=0, column=1, pady=10)
 
-        next_month_btn = customtkinter.CTkButton(
+        self.next_month_btn = customtkinter.CTkButton(
             self,
             text="next",
             width=60,
             command=calendar_obj.increase_date
             )
-        next_month_btn.grid(row=0, column=2, padx=(50, 0), pady=10)
+        self.next_month_btn.grid(row=0, column=2, padx=(50, 0), pady=10)
